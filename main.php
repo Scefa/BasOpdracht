4e098578f9ac0d5927247222fb8d56cf6e3dff22
ali <9018698@student.zadkine.nl>
2024-03-08 15:43:23 +0100

    Update main.php

--- main.php
+++ main.php
@@ -14,24 +14,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="index.css" />
+    
 </head>
 <body>
 <header class="homeheader">
-      <div class="logos">
-        <img class="homelogo" src="#" alt="a" />
-        <img class="homelogo" src="#" alt="a" />
-      </div>
+      
+<a href="main.php" class="button-container">
+  <img class="homelogo" src="./img/logobas.png" alt="Logo">
+</a>
+
+        
+   
 
       <nav class="homenav">
         <a href="main.php">Home</a>
         <a href="contact.php">Contact</a>
         <a href="loginpage.php">Login</a>
       </nav>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <form class="d-flex" role="search" action="search.php" method="post">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_query">
+    <button class="btn btn-outline-success" type="submit" name="search_submit">Search</button>
+</form>
+
     </header>
     <div class="homeline"></div>
 
