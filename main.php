3c355405914f33200e0bf0623405ad3ced3aa42b
ali <9018698@student.zadkine.nl>
2024-03-08 13:42:56 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,4 +1,19 @@
 <?php 
 
-echo "halo";
+
+
+
 ?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<h1>assda</h1>
+</body>
+</html>
