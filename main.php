2b89fdb5f41e15106b2846225d70bbe78623edc7
ali <9018698@student.zadkine.nl>
2024-03-08 14:28:27 +0100

    Update main.php

--- main.php
+++ main.php
@@ -11,14 +11,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
 </head>
 <body>
-    
+<header class="homeheader">
+      <div class="logos">
+        <img class="homelogo" src="#" alt="a" />
+        <img class="homelogo" src="#" alt="a" />
+      </div>
 
-<<<<<<< Updated upstream
-<h1>sikim</h1>
-=======
-<h1>assda   </h1>
->>>>>>> Stashed changes
+      <nav class="homenav">
+        <a href="main.php">Home</a>
+        <a href="contact.php">Contact</a>
+      </nav>
+
+    </header>
+    <div class="homeline"></div>
+
+
+
+
+
 </body>
 </html>
