a968cbfd1a380afd2c4a80459ce06a8fee288a80
ali <9018698@student.zadkine.nl>
2024-03-08 13:56:27 +0100

    Update main.php

--- main.php
+++ main.php
@@ -6,6 +6,7 @@
 ?>
 
 <!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,6 +15,10 @@
 <body>
     
 
+<<<<<<< Updated upstream
 <h1>sikim</h1>
+=======
+<h1>assda   </h1>
+>>>>>>> Stashed changes
 </body>
 </html>
