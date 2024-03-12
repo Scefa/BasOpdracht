3cd4015591e2a5c99fa6c8e57675be2f48d93f3b
ali <9018698@student.zadkine.nl>
2024-03-12 15:29:01 +0100

    Update login_pagina.index.php

--- login_pagina.index.php
+++ login_pagina.index.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Login & Registration Form</title>
-  <form action="main.php" method="post">
-
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -15,35 +12,33 @@
     <input type="checkbox" id="check">
     <div class="login form">
       <header>Login</header>
-      <form action="#">
-        <input type="text" placeholder="Enter your email">
-        <input type="password" placeholder="Enter your password">
+      <form action="login_pagina.index.php" method="post"> 
+        <input type="text" name="email" placeholder="Enter your email" required> 
+        <input type="password" name="password" placeholder="Enter your password" required> 
         <a href="#">Forgot password?</a>
-        <input type="button" class="button" value="Login">
+        <input type="submit" class="button" value="Login"> 
       </form>
       <div class="signup">
         <span class="signup">Don't have an account?
          <label for="check">Signup</label>
         </span>
       </div>
-</div>
+    </div>
 
-<div class="registration form">
-    <header>Signup</header>
-    <form action="login_pagina.index.php" method="post">
-       
+    <div class="registration form">
+      <header>Signup</header>
+      <form action="insert_data.php" method="post">
         <input type="text" name="email" placeholder="Enter your email" required>
         <input type="password" name="password" placeholder="Create a password" required>
         <input type="password" name="confirm_password" placeholder="Confirm your password" required>
         <input type="submit" class="button" value="Signup" name="submit">
-
-    </form>
-    <div class="signup">
+      </form>
+      <div class="signup">
         <span class="signup">Already have an account?
-            <label for="check"><a href="login_pagina.index.php">Login</a></label>
+          <label for="check"><a href="login_pagina.index.php">Login</a></label>
         </span>
+      </div>
     </div>
-</div>
-
+  </div>
 </body>
 </html>
