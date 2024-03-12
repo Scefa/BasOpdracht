a4ecef2fc9cc22ed2862ed525e5baae130020458
ali <9018698@student.zadkine.nl>
2024-03-12 15:09:46 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,47 +1,40 @@
-<?php 
-
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css" />
-    
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login & Registration Form</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<header class="homeheader">
-      
-<a href="main.php" class="button-container">
-  <img class="homelogo" src="./img/logobas.png" alt="Logo">
-</a>
+  <div class="container">
+    <input type="checkbox" id="check">
+    <div class="login form">
+      <header>Login</header>
+      <form action="main.php" method="post">
+        <input type="text" name="login_email" placeholder="Enter your email">
+        <input type="password" name="login_password" placeholder="Enter your password">
+        <a href="#">Forgot password?</a>
+        <input type="submit" class="button" value="Login">
+      </form>
+      <div class="signup">
+        <label for="check">Don't have an account? Signup</label>
+      </div>
+    </div>
 
-        
-   
-
-      <nav class="homenav">
-        <a href="main.php">Home</a>
-        <a href="contact.php">Contact</a>
-        <a href="loginpage.php">Login</a>
-      </nav>
-      <form class="d-flex" role="search" action="search.php" method="post">
-    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_query">
-    <button class="btn btn-outline-success" type="submit" name="search_submit">Search</button>
-</form>
-
-    </header>
-    <div class="homeline"></div>
-
-
-
-
-
+    <div class="registration form">
+      <header>Signup</header>
+      <form action="login_pagina.index.php" method="post">
+        <input type="text" name="email" placeholder="Enter your email" required>
+        <input type="password" name="password" placeholder="Create a password" required>
+        <input type="password" name="confirm_password" placeholder="Confirm your password" required>
+        <input type="submit" class="button" value="Signup" name="submit">
+      </form>
+      <div class="signup">
+        <label for="check">Already have an account? <a href="login_pagina.index.php">Login</a></label>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
