d4710ec31f5da5da01e3388fa74ee91956aca28e
ali <9018698@student.zadkine.nl>
2024-03-12 15:52:13 +0100

    Update main.php

--- main.php
+++ main.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="index.css" />
+
     
 </head>
 <body>
@@ -24,7 +24,7 @@
       <nav class="homenav">
         <a href="main.php">Home</a>
         <a href="contact.php">Contact</a>
-        <a href="loginpage.php">Login</a>
+        <a href="login_pagina.index.php">Logout</a>
       </nav>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
