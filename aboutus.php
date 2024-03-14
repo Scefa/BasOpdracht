e936c8dd6e2561ddf77503cee97c0a1c6340ea71
Berkaypz <112856956+Berkaypz@users.noreply.github.com>
2024-03-14 11:17:41 +0100

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
+    
+</head>
+<body>
+<header class="homeheader">
+      
+<a href="main.php" class="button-container">
+  <img class="homelogo" src="./img/logobas.png" alt="Logo">
+</a>
+      <nav class="homenav">
+        <a href="main.php">Home</a>
+        <a href="contact.php">Contact</a>
+        <a href="loginpage.php">Login</a>
+        <a href="aboutus.php">About us</a>
+      </nav>
+      <form class="d-flex" role="search" action="search.php" method="post">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_query">
+    <button class="btn btn-outline-success" type="submit" name="search_submit">Search</button>
+</form>
+
+    </header>
+    <div class="homeline"></div>
+
+
+
+
+
+</body>
+</html>
